3a9ddbf5db8d1cf1c5afff8eae280ac191c53ce1
zakiya123 <zakiyams2505@gmail.com>
2022-01-29 02:46:47 -0800

    first commit

---
+++ myproject/src/myproject/AreaOfTriangle.java
@@ -0,0 +1,17 @@
+package myproject;
+
+import java.util.Scanner;
+
+public class AreaOfTriangle {
+
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		System.out.println("Enter a with of a triangle...");
+		double width=scan.nextDouble();
+		System.out.println("Enter a side of a triangle...");
+		double side=scan.nextDouble();
+		double area=(width*side)/2;
+		System.out.println(area);
+	}
+
+}
---
+++ myproject/src/myproject/HelloWorld.java
@@ -0,0 +1,7 @@
+package myproject;
+
+public class HelloWorld {
+		public static void main(String[] args) {
+			
+		}
+}
