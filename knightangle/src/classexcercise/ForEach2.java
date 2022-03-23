050e383ba94bb4e928df79ab05730c1912e9ae83
zakiya123 <zakiyams2505@gmail.com>
2022-03-23 11:55:39 -0700

    comit1

--- knightangle/src/classexcercise/ForEach2.java
+++ knightangle/src/classexcercise/ForEach2.java
@@ -1,5 +1,16 @@
 package classexcercise;
 
 public class ForEach2 {
+	 static void main(String[] args) {
+		int sum=0;
+		int nums[]= {1,2,3,4,5,6,7,8,9,10};
+		for(int x:nums) {
+			System.out.println("Value is:"+x);
+			sum+=x;
+			if(x==5)
+				break;
+			System.out.println("Summation of first 5 elements:"+sum);
+		}
 		
+	}	
 }
